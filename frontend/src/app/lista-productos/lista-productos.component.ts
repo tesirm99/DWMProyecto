3d7fbc1353fd6e165b033114a091304e29f04062
zaksif <87914532+zaksif@users.noreply.github.com>
2023-01-21 18:25:30 +0100

    Update lista-productos.component.ts

--- frontend/src/app/lista-productos/lista-productos.component.ts
+++ frontend/src/app/lista-productos/lista-productos.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lista-productos',
-  templateUrl: './lista-productos.component.html',
-  styleUrls: ['./lista-productos.component.css']
+  templateUrl: 'lista-productos.component.html',
+  styleUrls: ['lista-productos.component.css']
 })
-export class ListaProductosComponent {
-
-}
+export class ListaProductosComponent implements OnInit {
+   
+  sort = 'desc';
+   ngOnInit(): void {
+    
+ 
+      
+   }
+   onSortUpdated(newSort: any): void {
+    this.sort = newSort;
+  }
+ }
