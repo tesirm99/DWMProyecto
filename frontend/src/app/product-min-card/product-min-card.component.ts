117f71a4c4e1908fe65fe1168f4941957367cb44
trm187 <trm187@inlumine.ual.es>
2023-01-30 18:20:50 +0100

    Added product min card component

---
+++ frontend/src/app/product-min-card/product-min-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-min-card',
+  templateUrl: './product-min-card.component.html',
+  styleUrls: ['./product-min-card.component.css']
+})
+export class ProductMinCardComponent {
+
+}
