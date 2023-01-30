7aaf135f581f99b9f369aea0e314c71281efefcb
trm187 <trm187@inlumine.ual.es>
2023-01-30 18:23:03 +0100

    Product min card blabla

--- frontend/src/app/product-min-card/product-min-card.component.ts
+++ frontend/src/app/product-min-card/product-min-card.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-product-min-card',
   templateUrl: './product-min-card.component.html',
   styleUrls: ['./product-min-card.component.css']
 })
 export class ProductMinCardComponent {
+
+  @Input() product: any;
+  @Input() parent: string = '';
+
+  class: string = 'card';
+  show: boolean = true;
+  constructor(private prodService: ProductService, private router: Router) { 
+    
+  }
+
+  ngOnInit(): void {
+    console.log(this.parent);
+    console.log(this.product);
+    
+  }
+
+  viewProd(): void {
+    console.log(this.product);
+    this.router.navigate(['/product', this.product._id]);
+  }
 
 }
